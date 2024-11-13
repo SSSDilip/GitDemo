8b8098e3e3ac4969a6797b7f9dccad89434c446c
SSSDilip <sssente00@gmail.com>
2024-11-13 16:59:21 +0530

    final commit by Asian Time Zone

---
+++ src/test/java/GitDemoTest/DemoGit.java
@@ -0,0 +1,14 @@
+package GitDemoTest;
+
+import org.testng.annotations.Test;
+
+public class DemoGit {
+	
+	@Test
+	public void Test() {
+		System.out.println("DemoGit Test ");
+		System.out.println("DemoGit Test 1");
+		System.out.println("DemoGit Test 2 ");
+	}
+
+}
