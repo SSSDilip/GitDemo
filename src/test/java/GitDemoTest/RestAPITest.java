f879764101ea54e93a23518a1a3ee2e8b85ab01b
SSSDilip <sssente00@gmail.com>
2024-11-13 11:16:26 +0530

    this is from Asian time zone

---
+++ src/test/java/GitDemoTest/RestAPITest.java
@@ -0,0 +1,23 @@
+package GitDemoTest;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	@Test
+	public void PostJira() {
+		
+		System.out.println("PostJira 1 ");
+		System.out.println("PostJira 2 ");
+		System.out.println("PostJira 3 ");
+	}
+	
+	@Test
+	public void deleteJira() {
+		
+		System.out.println("Delete jira ");
+		//System.out.println("PostJira 1 ");
+		//System.out.println("PostJira 1 ");
+	}
+	
+
+}
